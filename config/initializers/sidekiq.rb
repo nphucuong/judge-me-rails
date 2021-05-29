438989605ea24a6dddda81378e182655af83fcb2
Cuong Nguyen <npcuon9@gmail.com>
2021-05-29 10:50:48 +0700

    Change from redisToGo to redis heroku

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,17 +1,15 @@
 require 'sidekiq'
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV["REDISTOGO_URL"] }
+  config.redis = { url: ENV['REDIS_URL'] }
 end
 
 Sidekiq.configure_server do |config|
   config.logger.level = Rails.logger.level
-  config.redis = { url: ENV["REDISTOGO_URL"] }
+  config.redis = { url: ENV['REDIS_URL'] }
 end
 
 # Config crontab with Sidekiq
 # https://github.com/ondrejbartas/sidekiq-cron#adding-cron-job
 schedule_file = 'config/schedule.yml'
-if File.exist?(schedule_file) && Sidekiq.server?
-  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
-end
+Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) if File.exist?(schedule_file) && Sidekiq.server?
